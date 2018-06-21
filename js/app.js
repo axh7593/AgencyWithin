7922fb0078e7084c852b0699421489d0b6b88a1a
Anthony Huff <axh7593@g.rit.edu>
2018-06-20 23:37:36 -0400

    6/20: Programmed more functionallity
    Continued to program funcitonallity into app. Developed the extended pages for each city by creating a HTML table showing a hourly forecast and then another table showing Daily forecast for the selected city

--- js/app.js
+++ js/app.js
@@ -1,23 +1,5 @@
 var api_key = "b02c829edbd50b430f4f974e9d24bb32";
 var api_url = "https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/";
-//var Buflatitude = "42.880230";
-//var Buflongitude = "-78.878738";
-var days = [
-    'Sunday',
-    'Monday',
-    'Tuesday',
-    'Wednesday',
-    'Thursday',
-    'Friday',
-    'Saturday'];
-var sunday    = [],
-			monday    = [],
-			tuesday   = [],
-			wednesday = [],
-			thursday  = [],
-			friday    = [],
-			saturday  = [];
-
 
 function createCall(latitude, longitude, cityName, divID){
     this.latitude = latitude;
@@ -35,12 +17,8 @@ function createCall(latitude, longitude, cityName, divID){
             "<strong>Current Temperature</strong>: " + currentTemp + "&#8457" + "<br>" + 
             "<strong>Current Conditions</strong>: " + summary;
         divID = document.querySelector(divID).innerHTML = string;
-    
-
       });
-      //console.log("normal lat: " + latitude);
 }
-
 
 
 function createExtended(latitude, longitude, cityName, divID){
@@ -53,11 +31,13 @@ function createExtended(latitude, longitude, cityName, divID){
 
     var city = cityName;
 
+
+    //create string for hourly forecast
     var string = "";
     string += "<h1 style='width: 400px;'>" + city + "</h1>" + "<br>";
-    string += "<h4 style='width: 400px;'>" + "Extended Hourly forecast" + "</h4>";
+    string += "<h4 style='width: 400px;'>" + "Hourly Forecast" + "</h4>";
     string += "<table style='width: 800px !important;'>" +
-                "<thead >" +
+                "<thead>" +
                     "<tr>" +
                         "<th>" + "Time" + "</th>" +
                         "<th>" + "Description" + "</th>"+
@@ -67,23 +47,22 @@ function createExtended(latitude, longitude, cityName, divID){
                         "<th>" + "Wind" + "</th>" +
                     "</tr>" +
                 "</thead>";
+
+
     $.getJSON(api_call, function(forecast) {
-        
-        
-        
-      
+            // loop through all the hourly data
             $.each(forecast.hourly.data, function(key, value){
-                console.log(value);
                 
                 // convert UNIX time to standard time stamps
                 var date = new Date(value.time * 1000);
                 var hours = date.getHours();
                 var minutes = "0" + date.getMinutes();
                 var formattedTime = hours + ":" + minutes.substr(-2);
-                //console.log(string);
                 
                 //convert precipitation to a percentage instead of a decimal
                 var precipPercent = value.precipProbability * 100;
+                // prevent value from creating a run on number by fixing to number to one decimal point
+                var precipFixed = precipPercent.toFixed(1);
 
                 //convert humidity to a percentage instead of a decimal also convert to string so that 
                 // substring can run on the results to shorten the result to prevent a runon percentage
@@ -101,19 +80,72 @@ function createExtended(latitude, longitude, cityName, divID){
                                     "<td>" + formattedTime + "</td>" +  
                                     "<td>" + value.summary + "</td>" +
                                     "<td>" + value.temperature + "&#8457" + "</td>" +
-                                    "<td>" + precipPercent + "%" + "</td>" + 
+                                    "<td>" + precipFixed + "%" + "</td>" + 
                                     "<td>" + humidPercentFixed + "%" + "</td>" +
-                                    "<td>" + value.windSpeed + " mph" + "</td>";
-                                     
+                                    "<td>" + value.windSpeed + " mph" + "</td>"
+                                "</tr>" +
+                            "</tbody>" +
+                        "</table>";          
+            }  
+        });
 
-            }
 
+
+
+    //create string for daily forecast
+    var dailyString = "";                
+    dailyString += "<table style='width: 900px !important; '>" +
+    dailyString + "<h4 style='width: 400px; padding-top: 30px;'>" + "Daily Forecast"  + "</h4>" +
+        "<thead >" +
+            "<tr>" +
+                "<th>" + "Date" + "</th>" +
+                "<th>" + "Description" + "</th>"+
+                "<th>" + "High/Low" + "</th>" +
+                "<th>" + "Precip" + "</th>" +
+                "<th>" + "Humidity" + "</th>" +
+                "<th>" + "Wind" + "</th>" +
+            "</tr>" +
+        "</thead>";
+
+
+            //loop through all the daily data
+            $.each(forecast.daily.data, function(key, value){
+                //console.log(value);
                 
-            });
-        //append above html to the selected divID parameter and change its inner html 
-        divID = document.querySelector(divID).innerHTML = string;
-    
+                // convert UNIX time to standard date stamps
+                var date = new Date(value.time * 1000);
+                // had to add 1 to month as data was showing the month as being 1 behind the current, could be api error?
+                var month = ((date.getMonth()) + 1).toString();
+                var day = (date.getDate()).toString();
+                var year = (date.getFullYear()).toString();
+                var formattedDate = month + "/" +  day + "/" + year;
+                
+                //convert precipitation to a percentage instead of a decimal
+                var precipPercent = value.precipProbability * 100;
+                // prevent value from creating a run on number by fixing to number to one decimal point
+                var precipFixed = precipPercent.toFixed(1);
 
+                //convert humidity to a percentage instead of a decimal also convert to string so that 
+                // substring can run on the results to shorten the result to prevent a runon percentage
+                var humidPercent = (value.humidity * 100).toString();
+                var humidPercentFixed = humidPercent.substring(0,5);
+                
+                // Format string that will be displayed on page as a html table
+                dailyString +="<tbody>" +
+                                "<tr>" +
+                                    "<td>" + formattedDate + "</td>" +  
+                                    "<td style='max-width:200px;'>" + value.summary + "</td>" +
+                                    "<td>" + value.temperatureMin + "&#8457" + " / " + value.temperatureMax + "&#8457" + "</td>" +
+                                    "<td>" + precipFixed + "%" + "</td>" + 
+                                    "<td>" + humidPercentFixed + "%" + "</td>" +
+                                    "<td>" + value.windSpeed + " mph" + "</td>"
+                                "</tr>" +
+                                "</tbody>" +
+                            "</table>";    
+            });
+
+        //append above html to the selected divID parameter and change its inner html and be sure to add both created strings so both tables appear
+        divID = document.querySelector(divID).innerHTML = string + dailyString;
       });
     
 }
